27fb32ef71e1a0f051c1f78a7081f0a209582190
Kaléu Caminha <kaleu.caminha@gmail.com>
2020-05-17 20:46:30 -0300

    Libera deploy via git para simplificar neste primeiro momento

---
+++ build/precache-manifest.b8b71a154ebee1dfe40771eac43d8d63.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e221032ab061cad83b6ce2bcddbde8",
+    "url": "/assets/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "cf3fdf7af60a294d6d3f48cb7ad82488",
+    "url": "/assets/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "a0e46feb3cc577478b127936e739dd08",
+    "url": "/assets/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d712b605ed58419c7e6d4ab885d147b7",
+    "url": "/assets/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "2f7ce797cf8f198dedb9a9f38b7ef13b",
+    "url": "/assets/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "ba817517b2c4e1ba1ce802c4d4fafdb4",
+    "url": "/assets/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "0cc9313edd6fc1db9e65",
+    "url": "/bundle.06c9a.css"
+  },
+  {
+    "revision": "0cc9313edd6fc1db9e65",
+    "url": "/bundle.0cc93.js"
+  },
+  {
+    "revision": "e797032b6a0c3afd5f2f9dbbec65a218",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a596390ebcee0e66821",
+    "url": "/polyfills.1a596.js"
+  },
+  {
+    "revision": "53d8fb8dab7d2a06f72e",
+    "url": "/route-About.chunk.53d8f.js"
+  },
+  {
+    "revision": "2aaa2f14f358034f2677",
+    "url": "/route-Home.chunk.2aaa2.js"
+  },
+  {
+    "revision": "2aaa2f14f358034f2677",
+    "url": "/route-Home.chunk.fa8ff.css"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "435527310af6e2bbca6662eae402eb05",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
